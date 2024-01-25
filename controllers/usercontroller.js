3d42a96307db088a8611691b519263be762ab2d2
RUDra200R <127923748+RUDra200R@users.noreply.github.com>
2024-01-25 15:45:57 +0530

    Add files via upload

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -34,3 +34,53 @@ exports.signup = async(req, res, next) =>{
 
 }
 
+//User login
+
+exports.login = async(req, res, next)=>{
+    try{
+        const {email, password} = req.body
+        if (!email || !password){
+            return res.status(401).json({ error : "Email and Password are required"})
+        }
+
+        // find a user base on email
+        const user = await prisma.user.findUnique({
+            where:{
+                email
+            }
+        })
+
+        // when there is no user
+        if(!user){
+            return res.status(401).json({ error : "User not found!"})
+        }
+
+
+        // password not match
+        if(user.password !== password)
+        {
+            return res.status(401).json({ error: 'Password does not match'})
+        }
+
+        // user is there and validation
+
+        cookieToken(user, res)
+
+    } catch(error) {
+        throw new Error(error)
+    }
+}
+
+// logout user
+
+exports.logout = async(req, res, next)=>{
+    try{
+        // clear the token from cookies
+        res.clearCookie('token');
+        res.json({
+            message:"Logged out successfully"
+        })
+    }catch(error){
+        throw new Error(error)
+    }
+}
